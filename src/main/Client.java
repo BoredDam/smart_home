5140cf0fff188b936e26f9bd32f2f18da8f29291
Damiano Trovato <damianotrovato122@gmail.com>
2025-06-06 19:20:33 +0200

    fixed client code

--- src/main/Client.java
+++ src/main/Client.java
@@ -7,7 +7,7 @@ import devices.speaker.BaseSpeaker;
 public class Client {
     public static void main(String[] args) {
         BaseSpeaker cassa = new BaseSpeaker("cassa");
-        SmartHomeController controller = new SmartHomeController();
+        SmartHomeController controller = SmartHomeController.getInstance();
         BaseSpeaker cassa2 = new BaseSpeaker("cass2");
         controller.addDevice(cassa);
         controller.addDevice(cassa2);
