c4b5ec208f3079a0215f27536877d7e2c22d52e8
Paolo Volpini <95240389+paolovolpini@users.noreply.github.com>
2025-06-19 15:27:04 +0200

    Ok now it's final cmon

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,23 +1,10 @@
 package main;
 
-import controller.SmartHomeController;
-import devices.speaker.BaseSpeaker;
-import devices.thermostat.Thermostat;
 import userFacade.GUIWindow;
 import userFacade.UserFacade;
 
 public class Client {
     public static void main(String[] args) throws InterruptedException {
-
-        BaseSpeaker cassa = new BaseSpeaker("cassa");
-        SmartHomeController controller = SmartHomeController.getInstance();
-        BaseSpeaker cassa2 = new BaseSpeaker("cass2");
-        controller.addDevice(cassa);
-        controller.addDevice(cassa2);
-        Thermostat thermostat = new Thermostat("thermostst");
-        thermostat.turnOn();
-        thermostat.setLowerBound(30);
-        controller.addDevice(thermostat);
         UserFacade menu = new UserFacade(new GUIWindow());
         menu.mainDialog();
     }
